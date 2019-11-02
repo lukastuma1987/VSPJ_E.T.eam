cd808e4b466e44614a72eeaf0f4565a17d9a0dd0
Johny446 <prycljan@gmail.com>
2019-11-02 01:44:50 +0100

    Zobrazení počtu článků v recenzním řízení

--- src/EditorialBundle/Repository/ArticleRepository.php
+++ src/EditorialBundle/Repository/ArticleRepository.php
@@ -3,7 +3,24 @@
 namespace EditorialBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use EditorialBundle\Entity\Magazine;
 
 class ArticleRepository extends EntityRepository
 {
+    public function countByMagazine(Magazine $magazine)
+    {
+        $query = $this->createQueryBuilder('a')
+            ->select('count(a)')
+            ->where('a.magazine = :magazine')
+            ->setParameter('magazine', $magazine)
+            ->getQuery()
+        ;
+
+        try {
+            return $query->getSingleScalarResult();
+        } catch (NonUniqueResultException $e) {
+            return 0;
+        }
+    }
 }
--- src/EditorialBundle/Twig/EditorialExtension.php
+++ src/EditorialBundle/Twig/EditorialExtension.php
@@ -2,15 +2,27 @@
 
 namespace EditorialBundle\Twig;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+use EditorialBundle\Entity\Article;
+use EditorialBundle\Entity\Magazine;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class EditorialExtension extends AbstractExtension
 {
+    /** @var ManagerRegistry */
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     public function getFilters()
     {
         return [
             new TwigFilter('operation', [$this, 'getOperation']),
+            new TwigFilter('countArticles', [$this, 'getArticlesCount']),
         ];
     }
 
@@ -19,5 +31,12 @@ class EditorialExtension extends AbstractExtension
         $exists = $entity && is_object($entity) && method_exists($entity, 'getId') && $entity->getId();
 
         return $exists ? 'Upravit' : 'Vytvořit';
+    }
+
+    public function getArticlesCount(Magazine $magazine)
+    {
+        $repository = $this->doctrine->getRepository(Article::class);
+
+        return $repository->countByMagazine($magazine);
     }
 }
